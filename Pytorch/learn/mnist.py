52766d9f1551885303538ca1068558b14661f24e
LinZui <yangminghe20@gmail.com>
2023-04-29 23:22:13 +0800

    yang

--- Pytorch/learn/mnist.py
+++ Pytorch/learn/mnist.py
@@ -4,22 +4,29 @@ from torch.utils.data import DataLoader
 import torch
 
 
+# 利用卷积神经网络的mnist数字识别
 # 定义网络结构
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.linear1 = nn.Linear(784, 10)
-        self.softmax = nn.Softmax(dim=1)
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 32, 5, 1, 2), nn.ReLU(), nn.MaxPool2d(2, 2))
+        self.conv2 = nn.Sequential(nn.Conv2d(32, 64, 5, 1, 2), nn.ReLU(), nn.MaxPool2d(2, 2))
+        self.fc1 = nn.Sequential(nn.Linear(3136, 1000), nn.Dropout(p=0.5), nn.ReLU())
+        self.fc2 = nn.Sequential(nn.Linear(1000, 10), nn.Softmax(dim=1))
 
     def forward(self, x):
-        # (64, 1, 28, 28) -> (64, 784)
+        # [64, 1, 28, 28]
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # [64, 63, 7, 7]
         x = x.view(x.size()[0], -1)
-        x = self.linear1(x)
-        x = self.softmax(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
         return x
 
 
 def train(train_loader, model, criterion, optimizer):
+    model.train()
     for i, data in enumerate(train_loader):
         # 获得一个批次的数据和标签
         inputs, labels = data
@@ -35,7 +42,8 @@ def train(train_loader, model, criterion, optimizer):
         optimizer.step()
 
 
-def test(test_loader, model):
+def test(test_loader, train_loader, model):
+    model.eval()
     correct = 0
     for i, data in enumerate(test_loader):
         # 获得一个批次的数据和标签
@@ -47,6 +55,18 @@ def test(test_loader, model):
         # 预测正确的数量
         correct += sum((predicted == labels)[:])
     print('Test acc:{0}'.format(correct / len(test_loader)))
+
+    correct = 0
+    for i, data in enumerate(train_loader):
+        # 获得数据和对应标签
+        inputs, labels = data
+        # 获得模型预测结果
+        out = model(inputs)
+        # 获得最大值,以及最大值所在的位置
+        _, predicted = torch.max(out, 1)
+        # 预测正确的数量
+        correct += sum((predicted == labels)[:])
+    print('Train acc:{0}'.format(correct / len(train_loader)))
 
 
 def main():
@@ -79,12 +99,12 @@ def main():
     # 定义代价函数
     criterion = nn.CrossEntropyLoss()
     # 定义优化器
-    optimizer = optim.SGD(model.parameters(), lr=0.5)
+    optimizer = optim.Adam(model.parameters(), lr=0.0003)
 
-    for epoch in range(10):
+    for epoch in range(20):
         print('epoch:', epoch)
         train(train_loader, model, criterion, optimizer)
-        test(test_loader, model)
+        test(test_loader, train_loader, model)
 
 
 if __name__ == '__main__':
