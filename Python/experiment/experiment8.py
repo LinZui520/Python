b23531ad06bb641531d1f74e80e9105027860677
linzui <yangminghe20@gmail.com>
2023-05-15 19:48:39 +0800

    kugou

--- Python/experiment/experiment8.py
+++ Python/experiment/experiment8.py
@@ -2,29 +2,20 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+url = 'https://www.kugou.com/songlist/gcid_3zjlycjqz1yz02a/'
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 "
+                  "Safari/537.36 Edg/113.0.1774.42"
+}
 
-def get_url_html(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58"
-    }
-    response = requests.get(url, headers=headers)
-    response.encoding = 'utf-8'
-    return response.text
+response = requests.get(url, headers=headers)
+html = response.text
+soup = BeautifulSoup(html, "html.parser")
 
+data = {}
+for item in soup.find_all('a', hidefocus="true"):
+    if re.findall('周杰伦', item['title']):
+        data[item['title']] = item['href']
 
-def main():
-    baseurl = 'https://movie.douban.com/top250?start='
-    movie = {}
-    for i in range(0, 10):
-        url = baseurl + str(i * 25)
-        html = get_url_html(url)
-        soup = BeautifulSoup(html, "html.parser")
-        for item in soup.find_all('div', class_="item"):
-            if item.find('span', class_="inq"):
-                movie[item.find('span', class_="title").text] = item.find('span', class_="inq").text
-    print(movie)
-
-
-if __name__ == '__main__':
-    main()
+for i in data:
+    print(i, data[i])
